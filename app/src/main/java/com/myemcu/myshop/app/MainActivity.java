af0475df6365f22d23875bb8185819a791b39599
myemcu <1142497680@qq.com>
2016-12-01 16:18:38 +0800

    V2，Butterknife初始化布局。

--- app/src/main/java/com/myemcu/myshop/app/MainActivity.java
+++ app/src/main/java/com/myemcu/myshop/app/MainActivity.java
@@ -1,15 +1,29 @@
 package com.myemcu.myshop.app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+import android.widget.RadioGroup;
 
 import com.myemcu.myshop.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    @Bind(R.id.frame_layout)
+    FrameLayout frameLayout;
+    @Bind(R.id.rg_main)
+    RadioGroup rgMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);     // 自动生成
+
+        rgMain.check(R.id.rb_home); // 用8.4.0的Butterknife这里要崩
     }
 }
